99ef60553b496ab9fc5b3d902428584165a1a233
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-04-20 23:51:27 +0700

    Update error

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -3,36 +3,27 @@
 namespace App\Http\Requests;
 
 use App\Traits\ApiResponse;
-use Dotenv\Validator;
-use Facade\FlareClient\Http\Response;
-use GuzzleHttp\Psr7\Response as Psr7Response;
-use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Client\Response as ClientResponse;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Http\Response as HttpResponse;
-use Illuminate\Support\Facades\Validator as FacadesValidator;
-use Illuminate\Validation\Validator as IlluminateValidationValidator;
-use PHPUnit\Util\Xml\Validator as XmlValidator;
-use Ramsey\Uuid\Rfc4122\Validator as Rfc4122Validator;
-
+use Illuminate\Http\Response;
 abstract class ApiRequest extends FormRequest
 {
     use ApiResponse;
 
     abstract public function rules();
 
-    protected function failedValidation(ValidationValidator $validator)
+    protected function failedValidation(Validator $validator)
     {
         throw new HttpResponseException($this->apiError($validator->errors(),
-            HttpResponse::HTTP_UNPROCESSABLE_ENTITY,
+            Response::HTTP_UNPROCESSABLE_ENTITY,
         ));
     }
 
     protected function failedAuthorization()
     {
         throw new HttpResponseException($this->apiError(null,
-            HttpResponse::HTTP_UNAUTHORIZED
+            Response::HTTP_UNAUTHORIZED
         ));
     }
 }
